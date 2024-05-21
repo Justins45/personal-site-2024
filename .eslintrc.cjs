76a9f92f28233314ed13bb5192fb2b79449cd2ce
Justin Shaw <jshaw@artsman.com>
2024-05-21 15:57:18 -0600

    add eslint formatting options

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,5 +11,17 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  "rules": {
+    "vue/multi-word-component-names": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_"
+      }
+    ],
   }
 }
