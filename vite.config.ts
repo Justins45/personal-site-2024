65a0567c7ff44ce65a95ccb7abf7d3db7d70c1b3
Justin Shaw <jshaw@artsman.com>
2024-05-21 15:57:52 -0600

    add unplugin-vue-router routing settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueRouter from "unplugin-vue-router/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter({
+      routesFolder: "./src/pages"
+    }),
     vue(),
   ],
   resolve: {
