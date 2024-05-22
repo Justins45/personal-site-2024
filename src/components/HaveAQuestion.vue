bdfde145953f66461c6a61e244fcb31d49fa407c
Justin Shaw <jshaw@artsman.com>
2024-05-22 16:41:06 -0600

    add have a question content

--- src/components/HaveAQuestion.vue
+++ src/components/HaveAQuestion.vue
@@ -3,5 +3,9 @@
 </script>
 
 <template>
-<h1>Have-a-question</h1>
+  <div class="my-5">
+    <h2 class="text-3xl font-bold text-center">Have a question?</h2>
+    <p class="text-lg font-medium text-center mt-3">Feel free to reach out, or
+    <RouterLink to="/faq" class="underline decoration-gold underline-offset-2">check out the FAQ</RouterLink> see if your question is there</p>
+  </div>
 </template>
