3b02712d2c402bfca4a3eadb2ee04801935d5777
Justin Shaw <jshaw@artsman.com>
2024-05-22 16:41:38 -0600

    update app.vue to hold footer and background base colour

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router"
 import Header from "@/components/Header.vue"
+import Footer from "@/components/Footer.vue"
 </script>
 
 <template>
-  <Header />
-  <div class="mx-auto max-w-7xl">
-    <RouterView />
+  <div class="bg-offwhite-500">
+    <Header />
+    <div class="mx-auto max-w-7xl">
+      <RouterView />
+    </div>
+    <Footer />
   </div>
 </template>
