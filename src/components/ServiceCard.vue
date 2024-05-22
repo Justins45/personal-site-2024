3fc11cf789a1099d15d16261943cc980c59b9bd0
Justin Shaw <jshaw@artsman.com>
2024-05-22 16:50:32 -0600

    add Service items into a component

---
+++ src/components/ServiceCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Component } from "vue"
+const props = defineProps<{
+  title: string,
+  icon: Component,
+  text: string
+}>()
+
+</script>
+
+<template>
+  <div class="mt-5">
+    <div class="bg-offblack-300 w-10/12 pb-5 px-3 mx-auto">
+      <div class="pl-3 pt-3 ml-2">
+        <component :is="props.icon" class="w-16 h-16 text-gold" />
+      </div>
+      <div class="ml-3 mt-5">
+        <h3 class="text-xl font-medium tracking-wider w-32 ml-2">{{ props.title }}</h3>
+        <div class="bg-gold rounded-full h-2 w-56 -mt-2.5"></div>
+        <p class="mt-5 ml-2">{{ props.text }}</p>
+      </div>
+    </div>
+  </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,7 @@
 import { PaintBrushIcon, ArrowRightIcon } from '@heroicons/vue/24/outline'
 import LetsWorkTogether from '@/components/LetsWorkTogether.vue'
 import HaveAQuestion from '@/components/HaveAQuestion.vue'
+import ServiceCard from '@/components/ServiceCard.vue'
 </script>
 
 <template>
@@ -22,19 +23,7 @@ import HaveAQuestion from '@/components/HaveAQuestion.vue'
     </div>
     <div class="bg-offblack-500 text-offwhite-500 py-5 mt-5">
       <h2 class="uppercase text-center text-2xl font-semibold tracking-wider">Services</h2>
-      <!-- TODO: Service item component Start -->
-      <div class="mt-5">
-        <div class="bg-offblack-300 w-10/12 pb-5 mx-auto">
-          <div class="pl-3 pt-3">
-            <PaintBrushIcon class="w-16 h-16 text-gold" />
-          </div>
-          <div class="ml-3 mt-5">
-            <h3 class="text-xl font-medium tracking-wider w-32">Website Design</h3>
-            <!-- TODO: accent bar -->
-            <p class="mt-5">Need a new design or a design in general? I can be the one-stop shop to have the design and the development of your website, or just the design/updates to a design if that is all you need.</p>
-          </div>
-        </div>
-      </div>
+      <ServiceCard title="Website Design" :icon="PaintBrushIcon" text="Need a new design or a design in general? I can be the one-stop shop to have the design and the development of your website, or just the design/updates to a design if that is all you need." />
     </div>
     <LetsWorkTogether />
     <div class="mt-5 mx-3">
@@ -56,7 +45,7 @@ import HaveAQuestion from '@/components/HaveAQuestion.vue'
         <p>My role was the team design lead for designing and building the project over the course of 5 months and managing 2 other design members</p>
       </div>
       <div class="my-5 ">
-        <RouterLink to="/projects" class="hover:underline underline-offset-2 decoration-gold flex justify-center">
+        <RouterLink to="/portfolio" class="hover:underline underline-offset-2 decoration-gold flex justify-center">
           <p class="text-2xl">View more projects</p>
           <ArrowRightIcon class="w-8 h-8 ml-3" />
         </RouterLink>
