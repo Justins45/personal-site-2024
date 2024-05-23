56a5229d25d8bc2ae8f572811e09cd072689783c
Justin Shaw <jshaw@artsman.com>
2024-05-23 15:45:47 -0600

    add footer icons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-import { LinkIcon } from "@heroicons/vue/24/outline"
+import { EnvelopeIcon } from "@heroicons/vue/24/outline"
+import { PhoneIcon } from '@heroicons/vue/24/solid'
+import Insagram from "@/assets/svgs/instagram.svg"
+import LinkedIn from "@/assets/svgs/linkedin.svg"
 
 // TODO: make footer links send to top of page when clicked
 
@@ -33,17 +36,25 @@ import { LinkIcon } from "@heroicons/vue/24/outline"
       <div>
         <!-- Icons -->
         <div class="mr-5 flex flex-col items-end space-y-5">
-          <a href="#" aria-label="email">
-            <LinkIcon class="h-8 w-8" />
+          <a href="mailto: shaw,justin@shaw.ca" aria-label="email">
+            <PhoneIcon class="h-8 w-8 text-offblack-500" />
           </a>
-          <a href="#" aria-label="phone">
-            <LinkIcon class="h-8 w-8" />
+          <a href="tel: +14039881047" aria-label="phone">
+            <EnvelopeIcon class="h-8 w-8 text-offblack-500" />
           </a>
-          <a href="#" aria-label="Instagram">
-            <LinkIcon class="h-8 w-8" />
+          <a
+            href="https://www.linkedin.com/in/justin-shaw-598786227/"
+            target="_blank"
+            aria-label="LinkedIn"
+          >
+            <img :src="LinkedIn" alt="linkedin svg" class="h-8 w-8 text-offblack-500" />
           </a>
-          <a href="#" aria-label="LinkedIn">
-            <LinkIcon class="h-8 w-8" />
+          <a
+            href="https://www.instagram.com/jx_shaw/"
+             target="_blank"
+             aria-label="Instagram"
+          >
+            <img :src="Insagram" alt="instagram svg" class="h-8 w-8 text-offblack-500" />
           </a>
         </div>
       </div>
