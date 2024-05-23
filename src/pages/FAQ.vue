ba16181c69a30bb8ff456ee37080414c0aebf1e6
Justin Shaw <jshaw@artsman.com>
2024-05-23 17:18:52 -0600

    add desktop styling

--- src/pages/FAQ.vue
+++ src/pages/FAQ.vue
@@ -5,19 +5,19 @@ import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue"
 
 <template>
   <div class="mx-3 mt-3">
-    <h1 class="text-3xl font-medium">Frequently Asked Questions</h1>
+    <h1 class="text-3xl md:text-5xl font-medium">Frequently Asked Questions</h1>
     <!-- TODO: Make Divider into a component -->
-    <div class="my-5">
+    <div class="my-5 md:my-10">
       <div class="mt-5 h-[1px] bg-offblack-300/30"></div>
       <div>
         <!-- TODO: Make Disclosure into a component ?? ~~Maybe~~ -->
         <Disclosure v-slot="{ open }">
           <DisclosureButton class="mt-3 flex w-full justify-between">
-            <span class="w-64 text-start text-2xl font-semibold">How much do you charge?</span>
-            <PlusIcon v-if="!open" class="mt-1 h-8 w-8" />
-            <MinusIcon v-if="open" class="mt-1 h-8 w-8" />
+            <span class="w-64 md:w-full text-start text-2xl md:text-3xl font-semibold">How much do you charge?</span>
+            <PlusIcon v-if="!open" class="mt-1 h-8 w-8 md:h-10 md:w-10" />
+            <MinusIcon v-if="open" class="mt-1 h-8 w-8 md:h-10 md:w-10" />
           </DisclosureButton>
-          <DisclosurePanel class="mt-2">
+          <DisclosurePanel class="mt-2 md:text-lg py-3">
             My pricing is based off how large of a project you will bring me, and how fast you need
             it done. A minimum price would be $600CAD for any website. Any additions on top would be
             additional.
@@ -28,13 +28,13 @@ import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue"
       <div>
         <Disclosure v-slot="{ open }">
           <DisclosureButton class="mt-3 flex w-full justify-between">
-            <span class="w-64 text-start text-2xl font-semibold"
+            <span class="w-64 md:w-full text-start text-2xl md:text-3xl font-semibold"
               >What is included in the minimum price?</span
             >
-            <PlusIcon v-if="!open" class="mt-1 h-8 w-8" />
-            <MinusIcon v-if="open" class="mt-1 h-8 w-8" />
+            <PlusIcon v-if="!open" class="mt-1 h-8 w-8 md:h-10 md:w-10" />
+            <MinusIcon v-if="open" class="mt-1 h-8 w-8 md:h-10 md:w-10" />
           </DisclosureButton>
-          <DisclosurePanel class="mt-2">
+          <DisclosurePanel class="mt-2 md:text-lg py-3">
             <div>
               <p>The minimum price includes:</p>
               <ul class="ml-5 list-disc">
@@ -50,13 +50,13 @@ import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue"
       <div>
         <Disclosure v-slot="{ open }">
           <DisclosureButton class="mt-3 flex w-full justify-between">
-            <span class="w-64 text-start text-2xl font-semibold"
+            <span class="w-64 md:w-full text-start text-2xl md:text-3xl font-semibold"
               >How long will it take to complete?</span
             >
-            <PlusIcon v-if="!open" class="mt-1 h-8 w-8" />
-            <MinusIcon v-if="open" class="mt-1 h-8 w-8" />
+            <PlusIcon v-if="!open" class="mt-1 h-8 w-8 md:h-10 md:w-10" />
+            <MinusIcon v-if="open" class="mt-1 h-8 w-8 md:h-10 md:w-10" />
           </DisclosureButton>
-          <DisclosurePanel class="mt-2">
+          <DisclosurePanel class="mt-2 md:text-lg py-3">
             The length of time to complete each projects depends on the schedule you need it
             completed within, the sooner you need it the sooner ill complete it, but for what is
             included in the minimums it will take about 2-3 weeks depending on the content being
@@ -68,13 +68,13 @@ import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue"
       <div>
         <Disclosure v-slot="{ open }">
           <DisclosureButton class="mt-3 flex w-full justify-between">
-            <span class="w-64 text-start text-2xl font-semibold"
+            <span class="w-64 md:w-full text-start text-2xl md:text-3xl font-semibold"
               >What do I need to have a website built?</span
             >
-            <PlusIcon v-if="!open" class="mt-1 h-8 w-8" />
-            <MinusIcon v-if="open" class="mt-1 h-8 w-8" />
+            <PlusIcon v-if="!open" class="mt-1 h-8 w-8 md:h-10 md:w-10" />
+            <MinusIcon v-if="open" class="mt-1 h-8 w-8 md:h-10 md:w-10" />
           </DisclosureButton>
-          <DisclosurePanel class="mt-2">
+          <DisclosurePanel class="mt-2 md:text-lg py-3">
             <div>
               <p>The checklist items that I would like you to have ready to go are:</p>
               <ul class="ml-5 list-disc">
