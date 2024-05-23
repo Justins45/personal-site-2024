15ee19cd35f7b56f660e2c00d602c01be358ca1a
Justin Shaw <jshaw@artsman.com>
2024-05-23 11:20:31 -0600

    add portfolio content

--- src/pages/portfolio/index.vue
+++ src/pages/portfolio/index.vue
@@ -1,5 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LetsWorkTogether from '@/components/LetsWorkTogether.vue'
+</script>
 
 <template>
-  <h1>Portfolio</h1>
+  <div >
+    <div class="mx-3 my-3">
+      <h1 class="font-semibold text-3xl">Portfolio Projects</h1>
+      <div class="mt-5">
+        <RouterLink to="/banfflogcabin">
+          <img src="" alt="Banff Log Cabin collage" class="mb-3">
+        </RouterLink>
+        <RouterLink to="/personalproject">
+          <img src="" alt="This current website collage" class="mb-3">
+        </RouterLink>
+        <RouterLink to="/components">
+          <img src="" alt="Artsman components collage" class="mb-3">
+        </RouterLink>
+      </div>
+    </div>
+    <LetsWorkTogether />
+    <div class="mx-3 my-3 space-y-5">
+      <img src="" alt="Gold mood board">
+      <img src="" alt="Maroon mood board">
+      <img src="" alt="Teal mood board">
+      <img src="" alt="Purple mood board">
+      <img src="" alt="Grey mood board">
+      <img src="" alt="Peach mood board">
+    </div>
+  </div>
 </template>
