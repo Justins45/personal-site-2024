7f8551014ad16c64645fb035c5c40a110aa7abb4
Justin Shaw <jshaw@artsman.com>
2024-05-23 11:32:17 -0600

    add view other projects component

---
+++ src/components/ViewOtherProjects.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ArrowLeftIcon } from '@heroicons/vue/24/outline'
+</script>
+
+<template>
+  <RouterLink to="/portfolio" class="flex flex-start hover:underline underline-offset-2 decoration-gold">
+    <ArrowLeftIcon class="w-6 h-6 mr-3" />
+    <p class="font-medium text-xl">View Other Projects</p>
+  </RouterLink>
+</template>
