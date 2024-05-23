274692c8df57c0543c72a50e22328b7dcd03993d
Justin Shaw <jshaw@artsman.com>
2024-05-23 17:31:57 -0600

    add tablet / desktop styling

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -6,28 +6,28 @@ import HaveAQuestion from "@/components/HaveAQuestion.vue"
 
 <template>
   <div class="mx-3 mt-3">
-    <h1 class="text-center text-5xl font-black">Let's Work Together</h1>
+    <h1 class="text-center text-5xl md:text-7xl lg:text-8xl md:w-96 lg:w-8/12 lg:my-10 md:mx-auto font-black">Let's Work Together</h1>
     <div class="mt-5 h-[1px] bg-offblack-300/30"></div>
-    <div class="mb-8 mt-5 text-center">
+    <div class="mb-8 mt-5 text-center md:flex md:justify-around md:mt-10">
       <div>
         <div class="flex-start mb-2 flex justify-center">
-          <PhoneIcon class="mr-1 mt-0.5 h-6 w-6" />
-          <h2 class="text-2xl font-semibold">Call / Text</h2>
+          <PhoneIcon class="mr-1 mt-0.5 h-6 w-6 md:mt-0 md:w-8 md:h-8" />
+          <h2 class="text-2xl md:text-3xl font-semibold">Call / Text</h2>
         </div>
         <a
           href="tel:+14039881047"
-          class="text-xl font-medium text-gold decoration-gold underline-offset-2 hover:underline"
+          class="text-xl md:text-2xl font-medium text-gold decoration-gold underline-offset-2 hover:underline"
           >(403) 988-1047</a
         >
       </div>
-      <div class="mt-5">
+      <div class="mt-5 md:mt-0">
         <div class="flex-start mb-2 flex justify-center">
-          <EnvelopeIcon class="mr-1 mt-0.5 h-6 w-6" />
-          <h2 class="text-2xl font-semibold">Email</h2>
+          <EnvelopeIcon class="mr-1 mt-0.5 h-6 w-6 md:mt-0 md:w-8 md:h-8" />
+          <h2 class="text-2xl md:text-3xl font-semibold">Email</h2>
         </div>
         <a
           href="mailto: shaw.justin@shaw.ca"
-          class="text-xl font-medium text-gold decoration-gold underline-offset-2 hover:underline"
+          class="text-xl md:text-2xl font-medium text-gold decoration-gold underline-offset-2 hover:underline"
           >shaw.justin@shaw.ca</a
         >
       </div>
