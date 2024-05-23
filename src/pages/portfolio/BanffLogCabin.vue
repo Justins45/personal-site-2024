5217c0dba1ac14fee82835821e7b8e274df76a91
Justin Shaw <jshaw@artsman.com>
2024-05-23 11:32:34 -0600

    add portfolio project descriptions

--- src/pages/portfolio/BanffLogCabin.vue
+++ src/pages/portfolio/BanffLogCabin.vue
@@ -1,5 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ViewOtherProjects from '@/components/ViewOtherProjects.vue'
+</script>
 
 <template>
-  <h1>Banff-Log-Cabin Project-view</h1>
+  <div class="mx-3">
+    <ViewOtherProjects />
+    <h1 class="text-3xl font-bold flex flex-col mt-3">
+      <span>Capstone -</span>
+      <span>Banff Log Cabin</span>
+    </h1>
+    <div class="mt-5 text-lg">
+      <p>Goal was to design and build a website for a self found client.</p>
+      <div class="my-3">
+        <p>Built with a team of 5 using</p>
+        <ul class="list-disc ml-5">
+          <li>React <span class="italic">(front end)</span></li>
+          <li>Strapi <span class="italic">(headless cms)</span></li>
+          <li>Stripe <span class="italic">(payment)</span></li>
+        </ul>
+      </div>
+      <p>
+        My role was the team design lead for designing and building the project over the course of
+        5 months and managing 2 other design members.
+      </p>
+    </div>
+    <img src="" alt="Banff Log Cabin Collage" class="mt-5">
+  </div>
 </template>
--- src/pages/portfolio/PersonalProject.vue
+++ src/pages/portfolio/PersonalProject.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ViewOtherProjects from '@/components/ViewOtherProjects.vue'
+</script>
 
 <template>
-  <h1>personal-project</h1>
+  <div class="mx-3">
+    <ViewOtherProjects />
+    <h1 class="text-3xl font-bold flex flex-col mt-3">
+      Personal Project
+    </h1>
+    <div class="mt-5 text-lg space-y-3">
+      <p>Goal was to design and build a new website for myself
+        acting as my digital resume.</p>
+      <p>
+        Building this website would replace the old site I had online since the middle of university. Built with Vue, this project takes what I had learned with Artsmanagment and applies that to outside the work space.
+      </p>
+    </div>
+    <img src="" alt="Personal Project Collage" class="mt-5">
+  </div>
 </template>
