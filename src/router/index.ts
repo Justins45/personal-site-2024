401cfbcec2be2f31320dec85dcf5f4e2fc0fcf08
Justin Shaw <jshaw@artsman.com>
2024-05-24 01:25:17 -0600

    added type any to remove type-check errors

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router/auto"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  scrollBehavior(to, from, savedPosition) {
+  scrollBehavior(to: any, from: any, savedPosition: any) {
     // always scroll to top
     return { top: 0 }
   }
