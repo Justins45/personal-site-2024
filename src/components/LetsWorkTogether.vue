ba00434247199facedb56ac1d9d77d1cb80120fd
Justin Shaw <jshaw@artsman.com>
2024-05-24 00:54:05 -0600

    add tablet / desktop styling

--- src/components/LetsWorkTogether.vue
+++ src/components/LetsWorkTogether.vue
@@ -3,11 +3,11 @@ import { ArrowRightIcon } from "@heroicons/vue/24/solid"
 </script>
 
 <template>
-  <div class="bg-offblack-500 px-3 py-5">
+  <div class="bg-offblack-500 px-3 py-5 md:py-16">
     <h2 class="text-center text-5xl font-bold text-gold">Let's Work Together</h2>
     <div class="mt-5 text-center text-offwhite-500">
       <RouterLink to="/contact">
-        <p class="text-xl tracking-wider underline-offset-2 hover:underline hover:decoration-gold">
+        <p class="text-xl tracking-wider underline-offset-2 hover:underline hover:decoration-gold md:flex justify-center">
           Get in touch and we will create something
           <span class="flex items-center justify-center">
             amazing <ArrowRightIcon class="ml-3 h-6 w-6 text-gold" />
