c343843da3a7283dec6af469ffb4b2480c8819e2
Justin Shaw <jshaw@artsman.com>
2024-05-24 00:49:07 -0600

    add desktop styling

--- src/components/HaveAQuestion.vue
+++ src/components/HaveAQuestion.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="my-5">
-    <h2 class="text-center text-3xl font-bold">Have a question?</h2>
-    <p class="mt-3 text-center text-lg font-medium">
+  <div class="my-5 md:my-10 md:pt-5">
+    <h2 class="text-center text-3xl font-bold md:text-4xl">Have a question?</h2>
+    <p class="mt-3 text-center text-lg md:text-xl md:w-96 md:mx-auto font-medium">
       Feel free to reach out, or
       <RouterLink to="/faq" class="underline decoration-gold underline-offset-2"
         >check out the FAQ</RouterLink
