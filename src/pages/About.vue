debdfc00881af977cc06ebf5e9444864f2942697
Justin Shaw <jshaw@artsman.com>
2024-05-23 20:49:11 -0600

    add tablet / desktop views

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -6,89 +6,95 @@ import JustinRiverRocks from "@/assets/images/justin-river-rocks.jpg"
 
 <template>
   <div>
-    <div class="">
-      <h1 class="ml-3 mt-3 text-xl">About the developer</h1>
-      <p class="text-center text-5xl font-black uppercase">Justin Shaw</p>
+    <div class="md:my-5">
+      <h1 class="ml-3 mt-3 text-xl md:text-3xl md:font-semibold">About the developer</h1>
+      <p class="text-center text-5xl font-black uppercase md:text-8xl">Justin Shaw</p>
     </div>
     <div class="relative">
-      <div class="h-24 w-full bg-offblack-500 absolute z-10 top-0 left-0"></div>
-      <img :src="JustinRiverRocks" alt="image of me crouching on rocks by a river" class="w-11/12 h-44 absolute z-20 mt-3 m-auto left-0 right-0 " />
-      <div class="bg-gold h-1.5 rounded-full w-20 absolute z-30 top-40"></div>
-      <div class="bg-gold h-1.5 rounded-full w-20 absolute z-30 top-36"></div>
-      <div class="bg-gold h-1.5 rounded-full w-20 absolute z-30 top-32"></div>
+      <div class="h-24 md:h-36 lg:h-56 w-full bg-offblack-500 absolute z-10 top-0 left-0"></div>
+      <div class="relative w-70">
+        <img :src="JustinRiverRocks" alt="image of me crouching on rocks by a river" class="h-32 md:h-60 lg:w-10/12 lg:h-auto absolute z-20 mt-3 lg:mt-5 m-auto left-0 right-0 " />
+        <div class="bg-gold h-1 md:h-1.5 rounded-r-full w-20 absolute z-30 top-20 md:top-44 lg:top-[338px] min-[470px]:w-44 lg:w-64"></div>
+        <div class="bg-gold h-1 md:h-1.5 rounded-r-full w-20 absolute z-30 top-24 md:top-48 lg:top-[370px] min-[470px]:w-44 lg:w-64"></div>
+        <div class="bg-gold h-1 md:h-1.5 rounded-r-full w-20 absolute z-30 top-28 md:top-52 lg:top-[402px] min-[470px]:w-44 lg:w-64"></div>
+      </div>
     </div>
-    <div class="h-16 w-full bg-offblack-500 mt-52"></div>
+    <div class="h-16 w-full bg-offblack-500 mt-40 md:mt-80  lg:mt-[525px]"></div>
     <div class="mx-3 space-y-5 text-xl mt-5">
-      <div class="ml-3">
-        <p class="text-sm">name</p>
-        <div class="-ml-3 h-[1px] bg-offblack-300/30"></div>
-        <div class="mt-2 space-y-2">
-          <p class="font-semibold">Justin Shaw</p>
-          <p class="font-semibold">Alberta, Canada</p>
+      <div class="md:flex">
+        <div class="ml-3 md:w-1/2">
+          <p class="text-sm">name</p>
+          <div class="-ml-3 h-[1px] bg-offblack-300/30"></div>
+          <div class="mt-2 space-y-2 md:mt-5">
+            <p class="font-semibold">Justin Shaw</p>
+            <p class="font-semibold">Alberta, Canada</p>
+          </div>
         </div>
-      </div>
-      <div class="ml-3">
-        <p class="text-sm">contact</p>
-        <div class="-ml-3 h-[1px] bg-offblack-300/30"></div>
-        <div class="mt-2 grid grid-cols-2">
-          <a href="#" class="font-semibold decoration-gold hover:underline">Instagram</a>
-          <a href="#" class="font-semibold decoration-gold hover:underline">LinkedIn</a>
-        </div>
-        <div class="mt-2 space-y-2">
-          <p>
-            <a
-              href="mailto: shaw.justin@shaw.ca"
-              class="font-semibold decoration-gold hover:underline"
+        <div class="ml-3 md:w-1/2">
+          <p class="text-sm">contact</p>
+          <div class="-ml-3 h-[1px] bg-offblack-300/30"></div>
+          <div class="mt-2 grid grid-cols-2 max-w-xs md:w-60 md:mt-5">
+            <a href="#" class="font-semibold decoration-gold hover:underline">Instagram</a>
+            <a href="#" class="font-semibold decoration-gold hover:underline">LinkedIn</a>
+          </div>
+          <div class="mt-2 space-y-2">
+            <p>
+              <a
+                href="mailto: shaw.justin@shaw.ca"
+                class="font-semibold decoration-gold hover:underline"
               >shaw.justin@shaw.ca</a
-            >
-          </p>
-          <p>
-            <a href="tel:+14039881047" class="font-semibold decoration-gold hover:underline"
+              >
+            </p>
+            <p>
+              <a href="tel:+14039881047" class="font-semibold decoration-gold hover:underline"
               >(403) 988-1047</a
-            >
-          </p>
+              >
+            </p>
+          </div>
         </div>
       </div>
-      <div class="ml-3">
-        <p class="text-sm">specialization</p>
-        <div class="-ml-3 h-[1px] bg-offblack-300/30"></div>
-        <div class="mt-2 space-y-2">
-          <p class="font-semibold">
-            Website components, site fragments, and UX/UI design. I have spent hundreds of hours
-            making easy-to-use pieces for large websites and projects, improving UX/UI design.
-          </p>
+      <div class="md:flex">
+        <div class="ml-3 md:w-1/2">
+          <p class="text-sm">specialization</p>
+          <div class="-ml-3 h-[1px] bg-offblack-300/30"></div>
+          <div class="mt-2 space-y-2 md:mt-5">
+            <p class="font-semibold md:mr-10">
+              Website components, site fragments, and UX/UI design. I have spent hundreds of hours
+              making easy-to-use pieces for large websites and projects, improving UX/UI design.
+            </p>
+          </div>
         </div>
-      </div>
-      <div class="ml-3">
-        <p class="text-sm">proficiencies</p>
-        <div class="-ml-3 h-[1px] bg-offblack-300/30"></div>
-        <div class="mt-2 grid grid-cols-2">
-          <p class="font-semibold">Tailwindcss</p>
-          <p class="ml-10 font-semibold">HTML5</p>
-          <p class="font-semibold">Vue 3</p>
-          <p class="ml-10 font-semibold">CSS3</p>
-          <p class="font-semibold">Javascript</p>
-          <p class="ml-10 font-semibold">Figma</p>
-          <p class="font-semibold">UX/UI Design</p>
-          <p class="ml-10 font-semibold">Wordpress</p>
+        <div class="ml-3 md:w-1/2">
+          <p class="text-sm">proficiencies</p>
+          <div class="-ml-3 h-[1px] bg-offblack-300/30"></div>
+          <div class="mt-2 grid grid-cols-2 md:mt-5">
+            <p class="font-semibold">Tailwindcss</p>
+            <p class="ml-10 font-semibold">HTML5</p>
+            <p class="font-semibold">Vue 3</p>
+            <p class="ml-10 font-semibold">CSS3</p>
+            <p class="font-semibold">Javascript</p>
+            <p class="ml-10 font-semibold">Figma</p>
+            <p class="font-semibold">UX/UI Design</p>
+            <p class="ml-10 font-semibold">Wordpress</p>
+          </div>
         </div>
       </div>
       <div class="ml-3">
         <p class="text-sm">bio</p>
         <div class="-ml-3 h-[1px] bg-offblack-300/30"></div>
-        <div class="mt-2 space-y-2">
+        <div class="mt-2 space-y-2 md:mt-5">
           <p class="font-semibold">
             My name is Justin, I am 22 years old website developer and I have been building websites
             for 4 years now. I specialize in
-            <a href="" class="underline decoration-gold">Vue</a> but I have also dabbled in
-            <a href="" class="underline decoration-gold">React</a> and built many websites in raw
+            <a href="" class="underline decoration-gold hover:text-gold">Vue</a> but I have also dabbled in
+            <a href="" class="underline decoration-gold hover:text-gold">React</a> and built many websites in raw
             HTML and CSS. Building a website that you will love is what gets me out of bed in the
             morning and I wish to make the best website possible for your needs.
           </p>
         </div>
       </div>
     </div>
-    <div class="my-5">
+    <div class="my-5 md:my-10">
       <LetsWorkTogether />
     </div>
   </div>
